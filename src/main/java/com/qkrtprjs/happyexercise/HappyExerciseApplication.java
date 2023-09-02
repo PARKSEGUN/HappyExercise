da1724bff2ddd7ca120027ed6176963e5d4ef8f4
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-09-02 11:13:00 +0900

    Initial commit

---
+++ src/main/java/com/qkrtprjs/happyexercise/HappyExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.qkrtprjs.happyexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qkrtprjs/happyexercise/HappyExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qkrtprjs.happyexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
